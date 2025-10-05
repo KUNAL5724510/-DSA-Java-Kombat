1a4510f22caa483d315bb07d47e1ea471ee35f53
kunal5724510 <kunal.sh.dev@gmail.com>
2025-10-05 12:27:33 +0530

    2

---
+++ Ashu Sharma/EorO.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EorO {
+     public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        int n=input.nextInt();
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+        input.close();
+
+         }    }
